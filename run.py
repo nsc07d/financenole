ba810446ad7bb970fe4b9b03e5e7e6cfef6ad1af
hmm14e <hmclaughlin96@yahoo.com>
2016-08-04 18:33:04 -0400

    Updates Email Section

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, url_for
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
 app = Flask(__name__)
 
@@ -32,6 +35,26 @@ def template():
         progress_bar += 37
     if c401K == u'Yes':
         progress_bar += 37
+        
+    msg = MIMEMultipart()
+    msg['From'] = 'financenole@gmail.com'
+    msg['To'] = str(email)
+    msg['Subject'] = 'FSU FinanceNole'
+    message = 'here is the email'
+    msg.attach(MIMEText(message))
+    
+    mailserver = smtplib.SMTP('smtp.gmail.com',587)
+    # identify ourselves to smtp gmail client
+    mailserver.ehlo()
+    # secure our email with tls encryption
+    mailserver.starttls()
+    # re-identify ourselves as an encrypted connection
+    mailserver.ehlo()
+    mailserver.login('financenole@gmail.com', 'GoNoles!')
+    
+    mailserver.sendmail('financenole@gmail.com', str(email), msg.as_string())
+    
+    mailserver.quit()
 
     return render_template('template.html', firstname=firstname, lastname=lastname, email=email,ef=ef, gmi=gmi, c401K=c401K, rent=rent, util=util, progress_bar=progress_bar)
 
