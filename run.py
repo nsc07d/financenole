a354f77e91f1a02fdfd049bb418a2f522fd8758f
nsc07d <nsc07d@my.fsu.edu>
2016-08-04 19:08:57 -0400

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template('index.html')
+    return render_template('index.html', my_string="Wheeeee!", my_list=[0, 1, 2, 3, 4, 5])
 
 
 @app.route("/login")
@@ -30,19 +30,28 @@ def template():
     gmi = request.form['gmi']
     c401K = request.form['c401K']
     rent = request.form['rent']
+    apr = request.form['apr']
+    if apr > 7:
+        progress_bar +=5
+    elif apr > 5:
+        progress_bar += 7
+    elif apr >= 2:
+        progress_bar += 10
     util = request.form['util']
     if ef == u'Yes':
-        progress_bar += 37
+        progress_bar += 40
     if c401K == u'Yes':
-        progress_bar += 37
-        
+        progress_bar += 40
+    if c401K == u'Yes' and ef == u'Yes' and apr < '4':
+        progress_bar = (progress_bar - 10) + 20
+
     msg = MIMEMultipart()
     msg['From'] = 'financenole@gmail.com'
     msg['To'] = str(email)
     msg['Subject'] = 'FSU FinanceNole'
     message = 'here is the email'
     msg.attach(MIMEText(message))
-    
+
     mailserver = smtplib.SMTP('smtp.gmail.com',587)
     # identify ourselves to smtp gmail client
     mailserver.ehlo()
@@ -51,12 +60,13 @@ def template():
     # re-identify ourselves as an encrypted connection
     mailserver.ehlo()
     mailserver.login('financenole@gmail.com', 'GoNoles!')
-    
+
     mailserver.sendmail('financenole@gmail.com', str(email), msg.as_string())
-    
+
     mailserver.quit()
 
-    return render_template('template.html', firstname=firstname, lastname=lastname, email=email,ef=ef, gmi=gmi, c401K=c401K, rent=rent, util=util, progress_bar=progress_bar)
+
+    return render_template('template.html', firstname=firstname, lastname=lastname, email=email,ef=ef, gmi=gmi, c401K=c401K, rent=rent, util=util, progress_bar=progress_bar, apr=apr)
 
 
 if __name__ == '__main__':
